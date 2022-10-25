ee6491e33ff81a680e7dd0be44592bf367dbe4dd
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-25 12:39:22 +0800

    create DSS-MS-REVIEW-V1

---
+++ DSS6-MS-REVIEW-V1/src/main/java/com/dss/review/DssMsReviewApplication.java
@@ -0,0 +1,13 @@
+package com.dss.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssMsReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssMsReviewApplication.class, args);
+	}
+
+}
---
+++ DSS6-MS-REVIEW-V1/src/test/java/com/dss/review/DssMsReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssMsReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
