d7df6e7065d1f779e3bde0bef3541b71e38d3c3c
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-25 12:38:01 +0800

    create DSS3-MS-LOGIN-V1

---
+++ DSS3-MS-LOGIN-V1/src/main/java/com/dss/auth/DssMsLoginApplication.java
@@ -0,0 +1,13 @@
+package com.dss.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssMsLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssMsLoginApplication.class, args);
+	}
+
+}
---
+++ DSS3-MS-LOGIN-V1/src/test/java/com/dss/auth/DssMsLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssMsLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
