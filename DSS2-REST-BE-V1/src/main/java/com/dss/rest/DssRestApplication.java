4e100abab65d184fe285d34c630b1a6332a556f1
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-25 12:37:44 +0800

    create DSS2-REST-BE-V1

---
+++ DSS2-REST-BE-V1/src/main/java/com/dss/rest/DssRestApplication.java
@@ -0,0 +1,13 @@
+package com.dss.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssRestApplication.class, args);
+	}
+
+}
---
+++ DSS2-REST-BE-V1/src/test/java/com/dss/rest/DssRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
