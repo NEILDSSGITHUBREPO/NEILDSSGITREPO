b8af139c4a7fa206ed25cf8744fa791739b79976
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-25 12:38:27 +0800

    create DSS4-MS-MOVIE-V1

---
+++ DSS4-MS-MOVIE-V1/src/main/java/com/dss/movie/DssMsMovieApplication.java
@@ -0,0 +1,13 @@
+package com.dss.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssMsMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssMsMovieApplication.class, args);
+	}
+
+}
---
+++ DSS4-MS-MOVIE-V1/src/test/java/com/dss/movie/DssMsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssMsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
