dbe31a97a9764870a91049c3ca9adc3232588a27
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-25 12:39:44 +0800

    DSS7-EUREKA-V1

---
+++ DSS7-EUREKA-V1/src/main/java/com/dss/registry/DssMsServiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.dss.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssMsServiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssMsServiceRegisteryApplication.class, args);
+	}
+
+}
---
+++ DSS7-EUREKA-V1/src/test/java/com/dss/registry/DssMsServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssMsServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
