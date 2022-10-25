b763ea68fe33e185b22780b978d7a8cceea68dd3
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-25 12:38:56 +0800

    create DSS50MS-ACTOR-V1

---
+++ DSS5-MS-ACTOR-V1/src/main/java/com/dss/actor/DssMsActorApplication.java
@@ -0,0 +1,13 @@
+package com.dss.actor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssMsActorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssMsActorApplication.class, args);
+	}
+
+}
