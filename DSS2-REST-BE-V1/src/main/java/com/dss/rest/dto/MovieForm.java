3628f87cf9d346ec3c8dd3549c5e565a032129b1
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-26 19:17:18 +0800

    create consumable movieform DTO

---
+++ DSS2-REST-BE-V1/src/main/java/com/dss/rest/dto/MovieForm.java
@@ -0,0 +1,52 @@
+package com.dss.rest.dto;
+
+import java.util.Set;
+
+public class MovieForm {
+
+    private String title;
+    private double budget;
+    private Set<String> categories;
+    private String releaseDate;
+    private String maturityRating;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public Set<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<String> categories) {
+        this.categories = categories;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMaturityRating() {
+        return maturityRating;
+    }
+
+    public void setMaturityRating(String maturityRating) {
+        this.maturityRating = maturityRating;
+    }
+}
