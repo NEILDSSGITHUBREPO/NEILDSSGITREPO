2fd9e2fc998bdca12bcd298ccf2f19a5b14a0fef
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-26 19:17:40 +0800

    create movieform dto to movie entity utility method

--- DSS2-REST-BE-V1/src/main/java/com/dss/rest/dto/util/DTOTransformer.java
+++ DSS2-REST-BE-V1/src/main/java/com/dss/rest/dto/util/DTOTransformer.java
@@ -1,25 +1,51 @@
 package com.dss.rest.dto.util;
 
+import com.dss.rest.dto.MovieForm;
 import com.dss.rest.dto.UserForm;
 import com.dss.rest.entity.User;
 import com.dss.rest.entity.UserInformation;
+import com.dss.rest.entity.types.Category;
+import com.dss.rest.entity.Movie;
+import com.dss.rest.entity.types.MaturityRating;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
 
 /*
-* Utility class to transform a DTO to Entity
-* */
+ * Utility class to transform a DTO to Entity
+ * */
 public class DTOTransformer {
 
     /*
-    * Transforms UserForm to User Entity
-    *
-    * @Param UserForm
-    * @Return User
-    * */
+     * Transforms UserForm to User Entity
+     *
+     * @Param UserForm
+     * @Return User
+     * */
     public static User transformToUser(UserForm userForm) {
         UserInformation userInformation = new UserInformation(userForm.getPhoneNumber(), userForm.getName());
 
         User user = new User(userForm.getEmail(), userForm.getPassword());
         user.setUserInformation(userInformation);
         return user;
+    }
+
+    public static Movie transformToMovie(MovieForm movieForm) {
+        Movie movie;
+
+        movie = new Movie(movieForm.getTitle()
+                , movieForm.getBudget()
+                , LocalDate.parse(movieForm.getReleaseDate()
+                , DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+
+        Set<Category> categories = new HashSet<>();
+
+        movieForm.getCategories().forEach(mfc -> categories.add(Category.valueOf(mfc.toUpperCase())));
+        movie.setCategories(categories);
+        movie.setMaturityRating(MaturityRating.valueOf(movieForm.getMaturityRating()));
+
+        return movie;
     }
 }
