58f8624942d1dc97bad0f53784d5cd6991eb534c
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-10-27 11:34:09 +0800

    Add new ValidationError Code VE05

--- DSS2-REST-BE-V1/src/main/java/com/dss/rest/dto/util/validator/ValidationError.java
+++ DSS2-REST-BE-V1/src/main/java/com/dss/rest/dto/util/validator/ValidationError.java
@@ -8,8 +8,9 @@ public enum ValidationError {
     SPECIAL_CHARACTER("VE01", "Special characters not allowed"),
     DUPLICATE_DATA("VE02", "Already in use"),
     FORMAT_MISMATCH("VE03", "Invalid Format"),
+    UNDEFINED_FIELD("VE04", "Undefined Value"),
 
-    UNDEFINED_FIELD("VE04", "Undefined Value");
+    UNSSUPORTED_RANGE("VE05", "Unsupported Range");
     private final String code;
     private final String description;
 
