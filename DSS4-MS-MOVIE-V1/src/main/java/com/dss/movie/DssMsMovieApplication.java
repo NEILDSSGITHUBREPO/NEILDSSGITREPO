a736fad8354017aa4c94a47e6fb56e0d14f0eee4
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-11-02 09:38:28 +0800

    movie eureka client enable

--- DSS4-MS-MOVIE-V1/src/main/java/com/dss/movie/DssMsMovieApplication.java
+++ DSS4-MS-MOVIE-V1/src/main/java/com/dss/movie/DssMsMovieApplication.java
@@ -2,8 +2,10 @@ package com.dss.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DssMsMovieApplication {
 
 	public static void main(String[] args) {
