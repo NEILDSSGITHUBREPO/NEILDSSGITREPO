39a68b675bacfa9f12b51ed6c3cdfbbc77a519a5
Neil Immanuel De Guzman <neilgithub25@gmail.com>
2022-11-08 11:22:39 +0800

    add junit for reviews micro-service

--- DSS6-MS-REVIEW-V1/src/test/java/com/dss/review/service/ReviewServiceTest.java
+++ DSS6-MS-REVIEW-V1/src/test/java/com/dss/review/service/ReviewServiceTest.java
@@ -4,6 +4,7 @@ import com.dss.review.dto.ReviewForm;
 import com.dss.review.dto.util.PageResult;
 import com.dss.review.entity.Movie;
 import com.dss.review.entity.Review;
+import com.dss.review.exception.FieldValidationException;
 import com.dss.review.exception.MovieNotFoundException;
 import com.dss.review.repository.MovieRepository;
 import com.dss.review.repository.ReviewRepository;
@@ -59,6 +60,48 @@ public class ReviewServiceTest {
     }
 
     @Test
+    public void shouldThrowFieldValidationExceptionOnUIDCreateReview(){
+        String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String rid = "5d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String uid = "afb1dc11-c222-4256-8c6d-ecc798acd303";
+        String wrong_uid = "afba1dc11-c222-4256-8c6d-ecc798acd303";
+
+        Review review = new Review();
+        review.setId(UUID.fromString(rid));
+
+        Mockito.when(movieRepository.findById(UUID.fromString(mvid)))
+                .thenReturn(Optional.of(new Movie()));
+
+        ReviewForm reviewForm = new ReviewForm();
+        reviewForm.setDescription("This is a review from test");
+        reviewForm.setRating((short) 2);
+
+        Assert.assertThrows(FieldValidationException.class
+                ,() -> reviewService.createReview(reviewForm, wrong_uid, mvid));
+    }
+
+    @Test
+    public void shouldThrowFieldValidationExceptionOnMVIDCreateReview(){
+        String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String rid = "5d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String uid = "afb1dc11-c222-4256-8c6d-ecc798acd303";
+        String wrong_mvid = "3dd1ba0ae-b055-4b31-80c9-31d7dc86a717";
+
+        Review review = new Review();
+        review.setId(UUID.fromString(rid));
+
+        Mockito.when(movieRepository.findById(UUID.fromString(mvid)))
+                .thenReturn(Optional.of(new Movie()));
+
+        ReviewForm reviewForm = new ReviewForm();
+        reviewForm.setDescription("This is a review from test");
+        reviewForm.setRating((short) 2);
+
+        Assert.assertThrows(FieldValidationException.class
+                ,() -> reviewService.createReview(reviewForm, uid, wrong_mvid));
+    }
+
+    @Test
     public void shouldGetMovieReview(){
         String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
         String rid = "5d1ba0ae-b055-4b31-80c9-31d7dc86a717";
@@ -78,6 +121,49 @@ public class ReviewServiceTest {
         Assert.assertNotNull(reviewService.viewMovieReview(mvid, rid));
     }
 
+    @Test
+    public void shouldThrowFieldValidationExceptionRIDOnGetMovieReview(){
+        String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String rid = "5d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String wrong_rid = "5d1ba0aae-b055-4b31-80c9-31d7dc86a717";
+
+        Review review = new Review();
+        review.setId(UUID.fromString(rid));
+
+        Movie movie = new Movie();
+        movie.setId(UUID.fromString(mvid));
+
+        Mockito.when(movieRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(movie));
+
+        Mockito.when(reviewRepository.findById(UUID.fromString(rid)))
+                .thenReturn(Optional.of(review));
+
+        Assert.assertThrows(FieldValidationException.class
+                , () ->reviewService.viewMovieReview(mvid, wrong_rid));
+    }
+
+    @Test
+    public void shouldThrowFieldValidationExceptionMVIDOnGetMovieReview(){
+        String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String rid = "5d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String wrong_mvid = "3d1baa0ae-b055-4b31-80c9-31d7dc86a717";
+
+        Review review = new Review();
+        review.setId(UUID.fromString(rid));
+
+        Movie movie = new Movie();
+        movie.setId(UUID.fromString(mvid));
+
+        Mockito.when(movieRepository.findById(any(UUID.class)))
+                .thenReturn(Optional.of(movie));
+
+        Mockito.when(reviewRepository.findById(UUID.fromString(rid)))
+                .thenReturn(Optional.of(review));
+
+        Assert.assertThrows(FieldValidationException.class
+                , () ->reviewService.viewMovieReview(wrong_mvid, rid));
+    }
     @Test
     public void shouldGetAllReviewOfMovie() {
         String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
@@ -106,6 +192,30 @@ public class ReviewServiceTest {
 
        Assert.assertThrows(MovieNotFoundException.class
                , () -> reviewService.viewMovieReviews(mvid, 1, 10, null, null));
+
+    }
+
+    @Test
+    public void shouldThrowFieldValidationErrorWhenGetAllReviewOfMovie() {
+        String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+
+        Mockito.when(movieRepository.existsById(eq(UUID.fromString(mvid))))
+                .thenReturn(Boolean.TRUE);
+
+        Assert.assertThrows(FieldValidationException.class
+                , () -> reviewService.viewMovieReviews(mvid, -1, 10, null, null));
+
+    }
+
+    @Test
+    public void shouldThrowFieldValidationErrorOfMVIDWhenGetAllReviewOfMovie() {
+        String mvid = "3d1ba0ae-b055-4b31-80c9-31d7dc86a717";
+        String wrong_mvid = "3d1baa0ae-b055-4b31-80c9-31d7dc86a717";
+        Mockito.when(movieRepository.existsById(eq(UUID.fromString(mvid))))
+                .thenReturn(Boolean.TRUE);
+
+        Assert.assertThrows(FieldValidationException.class
+                , () -> reviewService.viewMovieReviews(wrong_mvid, 1, 10, null, null));
 
     }
     @TestConfiguration
